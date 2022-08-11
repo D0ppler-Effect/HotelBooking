eb558126e7a7b4d947386f1419a51d21f78dcb59
Alexander Tkachenko <Alexander.Tkachenko@kaspersky.com>
2022-08-11 23:11:59 +0300

    Added unit test project

---
+++ HotelBooking.Tests/LiteDatabaseTestsBase.cs
@@ -0,0 +1,17 @@
+﻿using LiteDB;
+
+namespace HotelBooking.Tests
+{
+	public class LiteDatabaseTestsBase : TestBase
+	{
+		[SetUp]
+		public void SetupDatabase()
+		{
+			Database = new LiteDatabase(DatabaseName);
+		}
+
+		protected ILiteDatabase Database { get; set; }
+
+		private const string DatabaseName = "TestDatabase.db";
+	}
+}
